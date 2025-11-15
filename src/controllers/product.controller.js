469d96fc59036a8c8b4c04214010a31f613ce40b
Foridul35962 <foridulislam35962@gmail.com>
2025-11-15 22:52:32 +0600

    debug add product and add update product function

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,13 +14,28 @@ export const addProduct = asyncHandler(async (req, res) => {
         category,
         brand,
         sizes,
-        color,
+        colors,
         collections,
         material,
         gender,
         images,
         tags
     } = req.body
+
+    if (
+        !name ||
+        !description ||
+        !price ||
+        !countInStock ||
+        !sku ||
+        !category ||
+        !Array.isArray(sizes) || sizes.length === 0 ||
+        !Array.isArray(colors) || colors.length === 0 ||
+        !collections ||
+        !Array.isArray(images) || images.length === 0
+    ) {
+        throw new ApiErrors(400, "All field are required")
+    }
 
     try {
         const product = new Products({
@@ -33,7 +48,7 @@ export const addProduct = asyncHandler(async (req, res) => {
             category,
             brand,
             sizes,
-            color,
+            colors,
             collections,
             material,
             gender,
@@ -41,7 +56,7 @@ export const addProduct = asyncHandler(async (req, res) => {
             tags,
             user: req.user._id
         })
-    
+
         await product.save()
 
         return res
@@ -52,4 +67,76 @@ export const addProduct = asyncHandler(async (req, res) => {
     } catch (error) {
         throw new ApiErrors(400, "product save failed")
     }
+})
+
+export const updateProduct = asyncHandler(async (req, res) => {
+    const {
+        name,
+        description,
+        price,
+        discountPrice,
+        countInStock,
+        sku,
+        category,
+        brand,
+        sizes,
+        colors,
+        collections,
+        material,
+        gender,
+        images,
+        tags,
+        isFeatured,
+        isPublished,
+    } = req.body
+
+    if (
+        !name ||
+        !description ||
+        !price ||
+        !countInStock ||
+        !sku ||
+        !category ||
+        !Array.isArray(sizes) || sizes.length === 0 ||
+        !Array.isArray(colors) || colors.length === 0 ||
+        !collections ||
+        !Array.isArray(images) || images.length === 0
+    ) {
+        throw new ApiErrors(400, "All field are required")
+    }
+
+    const { _id: productId } = req.params
+    const product = await Products.findById(productId)
+    if (!product) {
+        throw new ApiErrors(404, "product not found")
+    }
+
+    product.name = name || product.name
+    product.description = description || product.description
+    product.price = price || product.price
+    product.discountPrice = discountPrice || product.discountPrice
+    product.countInStock = countInStock || product.countInStock
+    product.category = category || product.category
+    product.brand = brand || product.brand
+    product.sizes = sizes || product.sizes
+    product.colors = colors || product.colors
+    product.collections = collections || product.collections
+    product.material = material || product.material
+    product.gender = gender || product.gender
+    product.images = images || product.images
+    product.tags = tags || product.tags
+    product.isFeatured = isFeatured !== undefined ? isFeatured : product.isFeatured
+    product.isPublished = isPublished !== undefined ? isPublished : product.isPublished
+    product.sku = sku || product.sku
+
+    const updatedProduct = await product.save()
+    if (!updatedProduct) {
+        throw new ApiErrors(500, "Product updated failed")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, updatedProduct, "product updated successfully")
+        )
 })
--- src/routers/product.route.js
+++ src/routers/product.route.js
@@ -6,6 +6,7 @@ import checkAdmin from '../middlewares/checkAdmin.js'
 const product = express.Router()
 
 product.post('/add-product', verifyJWT, checkAdmin, productController.addProduct)
+product.put('/update-product/:_id', verifyJWT, checkAdmin, productController.updateProduct)
 
 
 export default product
