c90d0abfd8652b82a8326928a7b93ceb111f5ec5
Foridul35962 <foridulislam35962@gmail.com>
2025-11-17 11:43:46 +0600

    update-quantity, delete and get cart functionality added

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -77,3 +77,103 @@ export const addCart = asyncHandler(async (req, res) => {
             )
     }
 })
+
+export const updateQuantity = asyncHandler(async (req, res) => {
+    const { productId, quantity, size, color } = req.body
+    const userId = req.user?._id
+
+    if (quantity < 1) {
+        throw new ApiErrors(400, 'user entered genetive quantity')
+    }
+
+    const product = await Products.findById(productId)
+    if (!product) {
+        throw new ApiErrors(404, 'product not found')
+    }
+
+    const cart = await Cart.findOne({ user: userId })
+    if (!cart) {
+        throw new ApiErrors(400, 'user has no cart')
+    }
+
+    const productIdx = cart.products.findIndex(
+        (p) =>
+            p.productId.toString() === productId &&
+            p.size === size &&
+            p.color === color
+    )
+
+    if (productIdx < 0) {
+        throw new ApiErrors(404, 'product in cart not found')
+    }
+
+    cart.products[productIdx].quantity = quantity
+    cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+    )
+
+    await cart.save()
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, cart, "cart in product quantity updated successfully")
+        )
+})
+
+export const deleteCart = asyncHandler(async (req, res) => {
+    const { productId, size, color } = req.body
+    const userId = req.user?._id
+
+    if (!productId) {
+        throw new ApiErrors(400, 'product id is required')
+    }
+
+    const cart = await Cart.findOne({ user: userId })
+
+    if (!cart) {
+        throw new ApiErrors(404, 'user has no cart')
+    }
+
+    const productIdx = cart.products.findIndex(
+        (p) => p.productId.toString() === productId &&
+            p.color === color &&
+            p.size === size
+    )
+
+    if (productIdx < 0) {
+        throw new ApiErrors(400, 'user has not its product in his cart')
+    }
+
+    //calculate new total product price
+    cart.totalPrice -= cart.products[productIdx].price * cart.products[productIdx].quantity
+
+    //delete product in cart
+    cart.products.splice(productIdx, 1)
+
+    try {
+        await cart.save()
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, {}, 'product in cart deleted successfully')
+            )
+    } catch (error) {
+        throw new ApiErrors(500, 'product in cart deleted failed', error)
+    }
+})
+
+export const getCart = asyncHandler(async (req, res) => {
+    const userId = req.user?._id
+    const cart = await Cart.findOne({ user: userId })
+    if (!cart) {
+        throw new ApiErrors(404, 'user has no cart')
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, cart, 'cart fetched successfully')
+        )
+})
--- src/routers/Cart.route.js
+++ src/routers/Cart.route.js
@@ -4,6 +4,10 @@ import verifyJWT from '../middlewares/VerifyJWT.js'
 
 const Cart = express.Router()
 
-Cart.post('/add-cart', verifyJWT, cartController.addCart)
+Cart.post('/add', verifyJWT, cartController.addCart)
+Cart.patch('/update-quantity', verifyJWT, cartController.updateQuantity)
+Cart.delete('/delete', verifyJWT, cartController.deleteCart)
+Cart.get('/get', verifyJWT, cartController.getCart)
+
 
 export default Cart
