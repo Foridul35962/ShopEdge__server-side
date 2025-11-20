32cdeb518a73f099f812cf1a0855b6c87a0d49dc
Foridul35962 <foridulislam35962@gmail.com>
2025-11-21 02:12:52 +0600

    admin order list, change order status, delete order added

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -3,10 +3,10 @@ import ApiErrors from "../utils/ApiErrors.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
-export const getOrder = asyncHandler(async(req, res)=>{
+export const getOrder = asyncHandler(async (req, res) => {
     const userId = req.user?._id
 
-    const orders = await Order.find({user:userId}).sort({createdAt:-1})
+    const orders = await Order.find({ user: userId }).sort({ createdAt: -1 })
 
     if (!orders) {
         throw new ApiErrors(404, 'orders is not found')
@@ -19,7 +19,7 @@ export const getOrder = asyncHandler(async(req, res)=>{
         )
 })
 
-export const getOrderDetails = asyncHandler(async(req, res)=>{
+export const getOrderDetails = asyncHandler(async (req, res) => {
     const orderId = req.params?._id
 
     const order = await Order.findById(orderId).populate('user', 'name email')
@@ -31,5 +31,76 @@ export const getOrderDetails = asyncHandler(async(req, res)=>{
         .status(200)
         .json(
             new ApiResponse(200, order, 'order details fatched successfully')
+        )
+})
+
+export const adminOrderList = asyncHandler(async (req, res) => {
+    const orders = await Order.find({}).populate('user', 'name email')
+    if (!orders) {
+        throw new ApiErrors(404, 'order list is empty')
+    }
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, orders, 'order fetched successfully')
+        )
+})
+
+export const changeOrderStatus = asyncHandler(async (req, res)=>{
+    const {orderId, newStatus} = req.body
+    if (!orderId || !newStatus) {
+        throw new ApiErrors(400, 'all fields are required')
+    }
+
+    const validStatus = ["Processing", "Shipped", "Delivered", "Cancelled"]
+    if (!validStatus.includes(newStatus)) {
+        throw new ApiErrors(400, 'status is not valid')
+    }
+
+    const order = await Order.findById(orderId)
+    if (!order) {
+        throw new ApiErrors(404, 'order is not found')
+    }
+
+    if (newStatus === 'Delivered') {
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
+    }
+    order.status = newStatus
+
+    try {
+        await order.save()
+    } catch (error) {
+        throw new ApiErrors(500, 'order status update failed')
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, order, 'order status updated successfully')
+        )
+})
+
+export const deleteOrder = asyncHandler(async(req, res)=>{
+    const {orderId} = req.body
+    if (!orderId) {
+        throw new ApiErrors(400, 'order id is required')
+    }
+
+    const order = Order.findById(orderId)
+    if (!order) {
+        throw new ApiErrors(404, 'order is not found')
+    }
+
+    try {
+        await order.deleteOne()
+    } catch (error) {
+        throw new ApiErrors(500, 'order delete failed')
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, {}, 'order delete successfully')
         )
 })
--- src/routers/order.route.js
+++ src/routers/order.route.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import * as orderController from '../controllers/order.controller.js'
 import verifyJWT from '../middlewares/VerifyJWT.js'
+import checkAdmin from '../middlewares/checkAdmin.js'
 
 const order = express.Router()
 
 order.get('/my-orders', verifyJWT, orderController.getOrder)
 order.get('/order-details/:_id', verifyJWT, orderController.getOrderDetails)
+order.get('/admin-orders-list', verifyJWT, checkAdmin, orderController.adminOrderList)
+order.patch('/change-order-status', verifyJWT, checkAdmin, orderController.changeOrderStatus)
+order.delete('/delete-order', verifyJWT, checkAdmin, orderController.deleteOrder)
 
 export default order
