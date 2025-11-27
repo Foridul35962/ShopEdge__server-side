bd8b277eb839751368842554b304470a39700d7c
Foridul35962 <foridulislam35962@gmail.com>
2025-11-27 13:56:56 +0600

    prev index add

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 import dotEnv from 'dotenv'
+dotEnv.config()
 import app from './src/app.js'
 import connectDB from './src/db/database.js'
-import serverless from 'serverless-http'
-dotEnv.config()
 
+// connectDB call
 const PORT = process.env.PORT || 3000
 
-const handler = serverless(app);
-
-export default async (req, res) => {
-    await connectDB();
-    return handler(req, res);
-};
-
-if (process.env.NODE_ENV !== 'production') {
-    connectDB().then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running locally on http://localhost:${PORT}`);
-        })
-    }).catch((error) => {
-        console.log('Server connection failed: ', error);
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`server is running on http://localhost:${PORT}`);
     })
-}
+}).catch((error)=>{
+    console.log('server connection failed: ',error);
+})
